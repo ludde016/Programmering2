3ad5600d1df32c6464484345fef458636c180a6f
ludde016 <ludde.swala@gmail.com>
2019-08-26 10:53:22 +0200

    Commit #5
    Arrayen tar in 3 olika ord (eller meningar) och sedan skriver ut dem i omvänd ordning. Använder mig av 2 for-loops som går i "varsin riktning" om man kan kalla det så

--- src/Repitition/Repitition2.java
+++ src/Repitition/Repitition2.java
@@ -6,7 +6,15 @@ public class Repitition2 {
 	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		String ord = input.nextLine();   
+		String[] ord = new String[3];
+		for (int i = 0; i < 3; i++) {
+			ord[i] = input.nextLine(); 
+		}
+		for (int i = 2; i > -1; i--) {
+			System.out.println(ord[i]);
+			
+		}
+		
 	}
 
 }
