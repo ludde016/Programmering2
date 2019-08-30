802301518d097243e16a6833fde277a332ec839b
ludde016 <ludde.swala@gmail.com>
2019-08-30 13:51:34 +0200

    Commit #7
    Gjorde klart repetitionsuppgift 6 och skrev ner det som Jaok undervisade i en ny class

--- src/Repitition/Person.java
+++ src/Repitition/Person.java
@@ -2,7 +2,13 @@ package Repitition;
 
 public class Person {
 	
+	String name;
+	int age;
+	
 	public Person(String name, int age) {
+		
+		this.name = name;
+		this.age = age;
 		
 	}
 
--- src/Repitition/Repetition6.java
+++ src/Repitition/Repetition6.java
@@ -1,5 +1,19 @@
 package Repitition;
 
 public interface Repetition6 {
+	
+	public static void main(String[] args) {
+		
+		Person person1 = new Person("Ludvig", 18); 
+		Person person2 = new Person("Raiwin", 17); 
+		
+		if (person1.age > person2.age) {
+			System.out.println(person1.name);
+			
+		}
+		else {
+			System.out.println(person2.name);
+		}
+	}
 
 }
