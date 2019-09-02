a573e6dcb10ce23ae4fc1bae0995e5551891cc07
ludde016 <ludde.swala@gmail.com>
2019-09-02 10:03:20 +0200

    Commit #1
    Har börjat på uppgifterna och har hittils skapat en scanner och mina variabler

---
+++ src/RepetitionsLäxa/Uppgift1.java
@@ -0,0 +1,17 @@
+package RepetitionsLäxa;
+
+import java.util.Scanner;
+
+public class Uppgift1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		int tal = input.nextInt();
+		int sum = 0;
+		while() {
+			
+		}
+	}
+
+}
