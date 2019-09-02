085b80a3e0eb21d5374dd1c3bd64d2dd9cc4d86f
ludde016 <ludde.swala@gmail.com>
2019-09-02 10:38:00 +0200

    Uppgift 2 Commit #2
    Tog bort all min kod för jag hade en while-loop istället för en metod som kallar sig själv

--- src/RepetitionsLäxa/Uppgift2.java
+++ src/RepetitionsLäxa/Uppgift2.java
@@ -7,17 +7,8 @@ public class Uppgift2 {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
-		String[] ord;
+		String sträng;
 		
-		while(true) {
-			ord = new String[3];
-			for (int i = 0; i < 3; i++) {
-				ord[i] = input.nextLine(); 
-			}
-			for (int i = 2; i > -1; i--) {
-				System.out.println(ord[i]);
-				
-			}
-		}
+		
 	}
 }
