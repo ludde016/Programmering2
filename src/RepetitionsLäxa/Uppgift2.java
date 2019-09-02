784a2ab5f7550365bbd31b2748114c83b2b1e2c2
ludde016 <ludde.swala@gmail.com>
2019-09-02 10:56:38 +0200

    Uppgift 2 Commit #3
    Kom lite långt utan någon commit men hittils har jag skapat två metoder utöver main-metoden. En för att göra om strängen den tar in till en char-array och en för att vända på ordningen av bokstäverna i char-arrayen. Det fungerar inte helt men snart bör det var klart

--- src/RepetitionsLäxa/Uppgift2.java
+++ src/RepetitionsLäxa/Uppgift2.java
@@ -3,12 +3,27 @@ package RepetitionsLäxa;
 import java.util.Scanner;
 
 public class Uppgift2 {
+	
+	static 	Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		String ordet = input.nextLine();
+		System.out.print(ord(ordet));
 		
-		Scanner input = new Scanner(System.in);
-		String sträng;
+	}
+	
+	public static char[] ord(String ordet) {
 		
+		char[] array = ordet.toCharArray();
 		
+		return array;
+	}
+	
+	public static void baklänges(char[] array) {
+		String ny = "";
+		for (int i = array.length - 1; i > 0; i--) {
+			ny = ny + array[i];
+		}
+		System.out.println(ny);
 	}
 }
