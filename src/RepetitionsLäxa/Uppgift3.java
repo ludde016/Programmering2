a89a298418bffdc199535f6a94933e892c7efbcc
ludde016 <ludde.swala@gmail.com>
2019-09-05 22:52:43 +0200

    Uppgift 2 Commit #4
    Gjorde klart uppgift 2. Märkte att mitt problem var att jag inte anropade metoderna på rätt sätt för att jag kom ihåg fel. Nu fungerar den som den ska

---
+++ src/RepetitionsLäxa/Uppgift3.java
@@ -0,0 +1,19 @@
+package RepetitionsLäxa;
+
+import java.util.Scanner;
+
+public class Uppgift3 {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		Object in = input.next();
+		ObjectArray(in);
+		
+	}
+	
+	public static void ObjectArray(Object in) {
+		
+	}
+
+}
