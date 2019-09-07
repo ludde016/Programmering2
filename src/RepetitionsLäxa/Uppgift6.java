136cc0318036b2e0e65dfbfae3df5af1170fb005
ludde016 <ludde.swala@gmail.com>
2019-09-07 17:36:31 +0200

    Uppgift 6 Commit #1
    Tyckte uppgift 5 kunde tolkas på många sätt så jag väntar med den. Gör uppgift 6 under tiden

---
+++ src/RepetitionsLäxa/Uppgift6.java
@@ -0,0 +1,19 @@
+package RepetitionsLäxa;
+
+import java.util.Scanner;
+
+public class Uppgift6 {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String ord = input.next();
+		anagram(ord);
+	}
+	
+	public static <T> String anagram(String ord) {
+		double iter = Math.pow(ord.length() - 1, 2); //Antalet iterrationer 
+		
+		return null;
+	}
+
+}
