008ab4d962b2690107922d10f75b4ed5bd9d65ec
ludde016 <ludde.swala@gmail.com>
2019-09-07 17:50:24 +0200

    Uppgift 6 Commit #2
    Har lagt ut det grundläggande till min kod med lite annat som jag tror kommer behövas. Tänker igenom exakt hur jag vill göra men det är lite klurigt

--- src/RepetitionsLäxa/Uppgift6.java
+++ src/RepetitionsLäxa/Uppgift6.java
@@ -11,7 +11,14 @@ public class Uppgift6 {
 	}
 	
 	public static <T> String anagram(String ord) {
-		double iter = Math.pow(ord.length() - 1, 2); //Antalet iterrationer 
+		double iter = Math.pow(ord.length() - 1, 2); //Antalet iterrationer
+		char[] array = ord.toCharArray(); 
+		for (double i = 0; i < iter; i++) {
+			for (int j = 0; j < ord.length() - 1; j++) {
+				
+			}
+			
+		}
 		
 		return null;
 	}
