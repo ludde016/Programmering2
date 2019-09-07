9fe3c95d1cf8ab99d4634892538ced6ea70d708e
ludde016 <ludde.swala@gmail.com>
2019-09-07 17:10:01 +0200

    Uppgift 4 Commit #2
    Ja, jag kopierade min kod från förra uppgiften. Vad ska du göra åt saken?
    ( ͡° ͜ʖ ͡°)

--- src/RepetitionsLäxa/Box.java
+++ src/RepetitionsLäxa/Box.java
@@ -1,5 +1,31 @@
 package RepetitionsLäxa;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Box {
+	static ArrayList<Object> List = new ArrayList<Object>();
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		while (true) {
+			Object x = input.next();
+				if (x.equals("exit")) {
+				break;
+			}
+			Array(x);
+		}
+		
+		System.out.println(List);
+
+	}
+
+	public static <T> ArrayList<Object> Array(Object x) { //Gör så att den kan ta in olika objekt
+		List.add(x);
+
+		return List;
+	}
+
 
 }
