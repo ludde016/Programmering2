60152438a72df67239c5e59c8c7ec9e471ee07e7
ludde016 <ludde.swala@gmail.com>
2019-09-08 16:52:43 +0200

    Uppgift 6 Commit #5
    Jag har nu avklarat uppgiften. Det jag gjorde sedan senaste commiten var att skapa en arraylist, skapa en metod som skickar in vare anagram som min tredje skapar och spara den. Jag gjorde också så att main-metoden börjar med att anropa sista metoden för att spara orden man användaren skriver in

--- src/RepetitionsLäxa/Uppgift6.java
+++ src/RepetitionsLäxa/Uppgift6.java
@@ -1,37 +1,49 @@
 package RepetitionsLäxa;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Uppgift6 {
 	
+	static ArrayList<String> result = new ArrayList<String>();
+	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		String ord = input.next();
-		anagram(ord);
+		SaveWord(ord);
+		anagrams(ord);
 	}
 	
-	public static <T> String anagram(String ord) {
+	public static <T> void anagrams(String ord) {
 		// double iter = Math.pow(ord.length() - 1, 2); //Antalet iterrationer
 		char[] array = ord.toCharArray(); 
 		char save;
-		for (double i = 0; i < ord.length(); i++) {
-			for (int j = 0; j < ord.length(); j++) {
+		for (double i = 0; i < ord.length() - 1; i++) {
+			for (int j = 0; j < ord.length() - 1; j++) {
 				save = array[j + 1];
 				array[j + 1] = array[j];
 				array[j] = save;
+				SaveWord(word(array));
+				
 			}
 			
 		}
 		
-		return null;
+		System.out.print(result);
 	}
 	
 	public static <T> String word(char[] array) { 
 		String anagram = "";
 		for (int i = 0; i < array.length; i++) {
-			ananagram = ananagram + array[i];
+			anagram = anagram + array[i];
 		}
-		return ananagram;
+		return anagram;
+	}
+	
+	public static <T> ArrayList SaveWord(String anagram) {
+		result.add(anagram);
+		return result;
+		
 	}
 
 }
