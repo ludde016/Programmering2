890c04b73b2b72438ad176c1de6e23c2824e1c8f
ludde016 <ludde.swala@gmail.com>
2019-09-09 10:39:53 +0200

    Uppgift 6 Commit #8
    Märkte att koden faktiskt inte tog med alla anagram, behöver tänka efter nu och skriva om koden

--- src/RepetitionsLäxa/Uppgift6.java
+++ src/RepetitionsLäxa/Uppgift6.java
@@ -4,22 +4,28 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Uppgift6 {
-	
+
 	static ArrayList<String> result = new ArrayList<String>();
-	
-	public static void main(String[] args) { //Notera att metoden inte är rekursiv...
+
+	public static void main(String[] args) { // Notera att metoden inte är rekursiv...
 		Scanner input = new Scanner(System.in);
 		String ord = input.next();
 		anagrams(ord);
 	}
-	/**Metoden fungerar på så sätt att den flyttar fram den första bokstaven ett steg och sedan sparar den det nya ordet
-	 * ,detta gör den tills att bokstaven som var i början hamnar i slutet och då börjar den om tills den har bildar alla anagram.
+
+	/**
+	 * Metoden fungerar på så sätt att den flyttar fram den första bokstaven ett
+	 * steg och sedan sparar den det nya ordet ,detta gör den tills att bokstaven
+	 * som var i början hamnar i slutet och då börjar den om tills den har bildar
+	 * alla anagram. Metoden skriver också ut ArrayListan med alla anagram när den
+	 * är klar
 	 * 
 	 * @param ord
-	 * Detta är det ord som användaren har valt att skriva ut alla anagram av
+	 *            Detta är det ord som användaren har valt att skriva ut alla
+	 *            anagram av
 	 */
 	public static <T> void anagrams(String ord) {
-		char[] array = ord.toCharArray(); 
+		char[] array = ord.toCharArray();
 		char save;
 		for (double i = 0; i < ord.length(); i++) {
 			for (int j = 0; j < ord.length() - 1; j++) {
@@ -27,38 +33,42 @@ public class Uppgift6 {
 				array[j + 1] = array[j];
 				array[j] = save;
 				SaveWord(word(array));
-				
+
 			}
-			
+
 		}
-		
+
 		System.out.print(result);
 	}
-	/**Metoden fungerar på sätt att den tar emot char-arrayen från förra metoden och bildar ett ord med alla dess bokstäver
+
+	/**
+	 * Metoden fungerar på sätt att den tar emot char-arrayen från förra metoden och
+	 * bildar ett ord med alla dess bokstäver
 	 * 
 	 * @param array
-	 * Detta är den char-arrayen med alla bokstäver
-	 * @return
-	 * Metoden returnerar ordet som är ett anagram
+	 *            Detta är den char-arrayen med alla bokstäver
+	 * @return Metoden returnerar ordet som är ett anagram
 	 */
-	public static <T> String word(char[] array) { 
+	public static <T> String word(char[] array) {
 		String anagram = "";
 		for (int i = 0; i < array.length; i++) {
 			anagram = anagram + array[i];
 		}
 		return anagram;
 	}
-	/**Metoden fungerar på så sätt att den tar in varje anagram och sparar det i en arraylist
+
+	/**
+	 * Metoden fungerar på så sätt att den tar in varje anagram och sparar det i en
+	 * arraylist
 	 * 
 	 * @param anagram
-	 * Detta är anagramet
-	 * @return
-	 * Den returnerar en arraylist med anagramen
+	 *            Detta är anagramet
+	 * @return Den returnerar en arraylist med anagramen
 	 */
 	public static <T> ArrayList SaveWord(String anagram) {
 		result.add(anagram);
 		return result;
-		
+
 	}
 
 }
