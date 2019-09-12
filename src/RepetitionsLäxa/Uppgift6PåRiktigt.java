6333c3e4007f9d67f31ce394062e6d0f9ce7f2a7
ludde016 <ludde.swala@gmail.com>
2019-09-12 18:36:17 +0200

    Uppgift 6 commit #10
    Kopierar mycket från min andra kod då detta blir väldigt liknande

--- src/RepetitionsLäxa/Uppgift6PåRiktigt.java
+++ src/RepetitionsLäxa/Uppgift6PåRiktigt.java
@@ -4,22 +4,37 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Uppgift6PÂRiktigt {
-	
+
 	static ArrayList<String> result = new ArrayList<String>();
+	static int index = 0;
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		String ord = "ord";
-		rekursiv(ord);
-		
+		char[] array = ord.toCharArray();
+		rekursiv(array);
+
 	}
-	
-	public static <T> void rekursiv(String ord) {
-		
-		
-		while(result.size() < 6) {
-			rekursiv(ord);
+
+	public static <T> void rekursiv(char[] array) {
+		char save;
+		for (int j = 0; j < 2; j++) {
+			save = array[j + 1];
+			array[j + 1] = array[j];
+			array[j] = save;
+
+			while (result.size() < 6) {
+				rekursiv(array);
+			}
+			System.out.println(result);
 		}
-		System.out.println(result);
-	} 
+	}
+
+	public static <T> String word(char[] array) {
+		String anagram = "";
+		for (int i = 0; i < array.length; i++) {
+			anagram = anagram + array[i];
+		}
+		return anagram;
+	}
 }
