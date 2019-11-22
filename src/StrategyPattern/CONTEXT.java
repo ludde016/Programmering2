34cbcd3c349f252ffa5c09910e9b32754d375afa
ludde016 <ludde.swala@gmail.com>
2019-11-22 13:23:02 +0100

    SOLID labb #1 Commit #3
    Gjorde klart CONTEXT koden. Den är samma som den de visar på hemsidan men nu fattar jag iallafall varför man gör som man gör

--- src/StrategyPattern/CONTEXT.java
+++ src/StrategyPattern/CONTEXT.java
@@ -6,4 +6,9 @@ public class CONTEXT {
 	public CONTEXT(StrategyInterface _strategy) {
 		_strategy = strategy; 
 	}
+	
+	public double executeStrategy(double num1, double num2) {
+		
+		return strategy.Calculate(num1, num2);
+	}
 }
